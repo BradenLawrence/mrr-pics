a64f679d912a84c880f37b1bda58d90444594fea
blip <b@bradenlawrence.com>
2019-10-07 15:08:27 -0400

    add axios and async await syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term)
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      headers: {
+        Authorization: 'Client-ID 621b2f5710892ce4a5712bef42087586a145553c579ec47094ffeed95903a4f8'
+      },
+      params: { query: term }
+    })
+    console.log(response.data.results)
   }
   
   render() {
