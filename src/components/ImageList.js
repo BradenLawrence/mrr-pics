358ae678d2e6a896166e715c9ab8e15e81d5ee30
blip <b@bradenlawrence.com>
2019-10-11 17:46:28 -0400

    add styles

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import './imageList.css'
 
 const ImageList = (props) => {
-  console.log(props.images)
-  return <div>Image List</div>
+  const imageList = props.images.map(({id, urls, alt_description}) => {
+    return <img key={id} src={urls.small} alt={alt_description} /> 
+  })
+  return <div className="image-list">{imageList}</div>
 }
 
 export default ImageList
