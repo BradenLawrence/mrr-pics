784810fd12bb3575abb75a945d2dc889147aceac
Braden Lawrence <b@Jens-MacBook-Pro.local>
2019-10-31 12:26:26 -0400

    add ImageCard to ImageList and replace vanilla img tags with it

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './imageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
   const imageList = props.images.map(({id, urls, alt_description}) => {
-    return <img key={id} src={urls.small} alt={alt_description} /> 
+    return <ImageCard key={id} src={urls.small} alt={alt_description} />
   })
   return <div className="image-list">{imageList}</div>
 }
