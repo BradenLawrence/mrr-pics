a0107da6770be408ff6cd2d3a563dfa196a876b8
Braden Lawrence <b@Jens-MacBook-Pro.local>
2019-11-08 13:52:18 -0500

    add image ref value

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.imageRef = React.createRef()
+  }
   render() {
     const {alt_description, urls} = this.props.image
     return (
       <div>
-        <img alt={alt_description} src={urls.small} />
+        <img ref={this.imageRef} alt={alt_description} src={urls.small} />
       </div>
     )
   }
